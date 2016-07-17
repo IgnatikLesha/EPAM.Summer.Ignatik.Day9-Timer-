480ad120ee66683b3ec9bce291d44e85295b2e57
Alexey Ignatik <ignatiklesha@gmail.com>
2016-07-17 17:58:06 +0300

    Add project files.

---
+++ TimeEvent/OutOfTimeEventArgs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeEvent
+{
+    public class OutOfTimeEventArgs: EventArgs
+    {
+        private string message;
+        private double time;
+
+        public string Message {
+            get { return message; }
+            set
+            {
+                if(ReferenceEquals(value, null))
+                    throw new ArgumentException();
+                message = value;
+            }
+        }
+
+        public double Time
+        {
+            get { return time; }
+            set
+            {
+                if(value < 0)
+                    throw new ArgumentException();
+                time = value;
+            }
+
+        }
+
+        public OutOfTimeEventArgs(string message, double time)
+        {
+            Message = message;
+            Time = time;
+        }
+    }
+}
---
+++ TimeEvent/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeEvent
+{
+    class Timer
+    {
+        public event EventHandler<OutOfTimeEventArgs> OutOfTime = delegate { };
+
+        private void OnOutOfTime(OutOfTimeEventArgs e)
+        {
+            OutOfTime(this, e);
+        }
+
+        public void SimulateOutOfTime(string message, double time)
+        {
+            OnOutOfTime(new OutOfTimeEventArgs(message, time));
+        }
+    }
+}
